d6656426f5a4336563d9c6a891b702811d448d05
Jose Saad <josemiguelsaad13@gmail.com>
2022-01-19 14:55:44 -0400

    efectos en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,57 +64,77 @@ function Home() {
     <div>
       <Header/>
       <Carrusel/>
-      <Fade>
+      <Fade bottom>
         <p className={classes.text2}>CONSULTA GRATIS PARA CADA UNO DE NUESTROS SERVICIOS</p>
       </Fade>
-      <Fade>
+      <Fade bottom>
         <div id="faciales" >
           <p className={classes.titleText}>FACIALES</p>
           <p className={classes.text1}>Lo mejor para tu rostro solo lo conseguiras aqui en Perfect Body Fast, contamos con:</p>
           <SecondCarrusel/>
         </div>
       </Fade>
-      <Fade>
+      <Fade bottom>
         <div id="corporales" >
           <p className={classes.titleText}>CORPORALES</p>
           <p className={classes.text1}>Lo mejor para tu cuerpo solo lo conseguiras aqui en Perfect Body Fast, contamos con:</p>
           <ThirdCarrusel/>
         </div>
       </Fade>
-      <Fade>
+      <Fade bottom>
         <div id="sobreNosotros" >
           <p className={classes.titleText}>SOBRE NOSOTROS</p>
           <p className={classes.text1}>Somos una organización dedicada a la medicina y cirugía estética con una atención profesional, especializado en cada área, amable y personalizada con cada uno de nuestros pacientes,  preparados para atenderlo en cualquier tipo de necesidad estética que requiera, ofrecemos tratamientos faciales y corporales, permitiéndoles mejorar su juventud, vitalidad y belleza,  con un alto grado de calidad, atendemos con dedicación y pasión.</p> 
           <img className={classes.imageNosotros} src="https://medica.orbitpublishers.com/wp-content/uploads/2021/06/guia-para-abrir-un-consultorio-medico.jpg"/>
-          <div id="ideales">
+          <div id="ideales" style={{marginTop: '10pt', marginBottom: '10pt'}}>
+           <Fade bottom>
             <p className={classes.subtitle1}>Misión</p>   
             <p className={classes.text1}>Brindar a nuestros pacientes un estilo de vida saludable e incrementar su esperanza de vida  a través  de  una  atención  personalizada por profesionales médicos para mejorar sus condiciones de salud, bienestar y belleza tanto física como psicológica a través del compromiso de nuestro equipo.</p>
+           </Fade>
+           <Fade bottom>
             <p className={classes.subtitle2}>Visión</p>   
             <p className={classes.text1}>Buscar ser la organización mas reconocida a nivel nacional e internacional  por su aporte  en  salud, bienestar, belleza, calidad humana y asesoría médica contando con excelentes procedimientos, tecnológicos y productos, con el fin de consolidarse en una entidad prestadora de servicios médicos estéticos, cada vez más integral.</p>
+           </Fade>
+           <Fade bottom>
             <p className={classes.subtitle1}>Valores</p>   
-            <List>
-              <ListItem>
-                <p className={classes.text1}>• Realizar nuestro trabajo con la pasión, dedicación y entusiasmo necesario para llevar a cabo nuestra misión.</p>
-              </ListItem>
-              <ListItem>
-                <p className={classes.text1}>• Honestidad, para transmitir confianza en todos los ámbitos ajustándonos siempre a la verdad.</p>
-              </ListItem>
-              <ListItem>
-                <p className={classes.text1}>• Respeto a nuestros pacientes mejorando su calidad de vida.</p>
-              </ListItem>
-              <ListItem>
-                <p className={classes.text1}>• Puntualidad para estar a tiempo y en el tiempo para satisfacción de nuestros pacientes.</p>
-              </ListItem>
-              <ListItem>
-                <p className={classes.text1}>• Amor para descubrir, aprender y compartir con cada uno de nuestros pacientes sus necesidades y disfrutarla por sus mejoras. </p>
-              </ListItem>
-              <ListItem>
-                <p className={classes.text1}>• Liderar,  motivar y compartir con nuestros pacientes la mejora de su calidad de vida. </p>
-              </ListItem>
-              <ListItem>
-                <p className={classes.text1}>• Confianza para darle la seguridad que su inquietud y necesidad será cubierta por nuestros profesionales a cada pacientes </p>
-              </ListItem>
+            <List style={{marginTop: '-10pt'}}>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Realizar nuestro trabajo con la pasión, dedicación y entusiasmo necesario para llevar a cabo nuestra misión.</p>
+                </ListItem>
+              </Fade>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Honestidad, para transmitir confianza en todos los ámbitos ajustándonos siempre a la verdad.</p>
+                </ListItem>
+              </Fade>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Respeto a nuestros pacientes mejorando su calidad de vida.</p>
+                </ListItem>
+              </Fade>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Puntualidad para estar a tiempo y en el tiempo para satisfacción de nuestros pacientes.</p>
+                </ListItem>
+              </Fade>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Amor para descubrir, aprender y compartir con cada uno de nuestros pacientes sus necesidades y disfrutarla por sus mejoras. </p>
+                </ListItem>
+              </Fade>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Liderar,  motivar y compartir con nuestros pacientes la mejora de su calidad de vida. </p>
+                </ListItem>
+              </Fade>
+              <Fade bottom>
+                <ListItem>
+                  <p className={classes.text1}>• Confianza para darle la seguridad que su inquietud y necesidad será cubierta por nuestros profesionales a cada pacientes </p>
+                </ListItem>
+              </Fade>
             </List>
+           </Fade>
           </div>
         </div>
       </Fade>
