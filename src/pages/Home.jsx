ede9ae14ada4bc5860e7c6cc6d19dd7d9d2cd28c
Jose Saad <josemiguelsaad13@gmail.com>
2022-01-20 17:31:48 -0400

    arreglado los ideales en pc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Carrusel from '../components/Carrusel';
 import Header from '../components/Header';
 import SecondCarrusel from '../components/SecondCarrusel';
 import Footer from '../components/Footer';
-import { makeStyles, List, ListItem } from "@material-ui/core";
+import { makeStyles, List, ListItem, Paper } from "@material-ui/core";
 import Fade from "react-reveal";
 import ThirdCarrusel from '../components/ThirdCarrusel';
 
@@ -17,6 +17,10 @@ const useStyles = makeStyles({
     margin: 'auto',
     "@media (max-width: 650pt)": {
       maxWidth: '95vw',
+    },
+    "@media (min-width: 650pt)": {
+      marginLeft: '10pt',
+      marginRight: '10pt',
     },
   },
   titleText: {
@@ -33,6 +37,10 @@ const useStyles = makeStyles({
     "@media (max-width: 650pt)": {
       maxWidth: '95vw',
     },
+    "@media (min-width: 1000pt)": {
+      width: '100%',
+      paddingTop: '10pt',
+    },
     color: '#8E7AA7',
     fontSize: '15pt',
   },
@@ -42,6 +50,10 @@ const useStyles = makeStyles({
     margin: 'auto',
     "@media (max-width: 650pt)": {
       maxWidth: '95vw',
+    },
+    "@media (min-width: 1000pt)": {
+      width: '100%',
+      paddingTop: '10pt',
     },
     color: '#83BAA6',
     fontSize: '15pt',
@@ -65,11 +77,43 @@ const useStyles = makeStyles({
       paddingBottom: '10pt',
     },
     paddingBottom: '40pt',
+  },
+  idealesCards: {
+    "@media (min-width: 1000pt)": {
+      display: 'inline-flex'
+    },
+  },
+  card: {
+    "@media (min-width: 1000pt)": {
+      width: '20vw',
+      height: '320pt',
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    marginBottom: '20pt',
+  },
+  card2: {
+    "@media (min-width: 1000pt)": {
+      width: '50vw',
+      marginLeft: '20pt',
+      marginRight: '20pt',
+      height: '320pt',
+    },
+    marginBottom: '20pt',
+  },
+  nosotrosContainer: {
+    "@media (min-width: 1000pt)": {
+      marginBottom: '20pt',
+    },
   }
 });
 
 function Home() {
   const classes = useStyles();
+  
+  const elevat = window.matchMedia("(min-width: 650pt)").matches ? 4 : 0;
+
   return (
     <div>
       <Header/>
@@ -92,58 +136,64 @@ function Home() {
         </div>
       </Fade>
       <Fade bottom>
-        <div id="sobreNosotros" >
+        <div className={classes.nosotrosContainer} id="sobreNosotros" >
           <p className={classes.titleText}>SOBRE NOSOTROS</p>
-          <p className={classes.text1}>Somos una organización dedicada a la medicina y cirugía estética con una atención profesional, especializado en cada área, amable y personalizada con cada uno de nuestros pacientes,  preparados para atenderlo en cualquier tipo de necesidad estética que requiera, ofrecemos tratamientos faciales y corporales, permitiéndoles mejorar su juventud, vitalidad y belleza,  con un alto grado de calidad, atendemos con dedicación y pasión.</p> 
+          <p className={classes.text3}>Somos una organización dedicada a la medicina y cirugía estética con una atención profesional, especializado en cada área, amable y personalizada con cada uno de nuestros pacientes,  preparados para atenderlo en cualquier tipo de necesidad estética que requiera, ofrecemos tratamientos faciales y corporales, permitiéndoles mejorar su juventud, vitalidad y belleza,  con un alto grado de calidad, atendemos con dedicación y pasión.</p> 
           <img className={classes.imageNosotros} src="https://medica.orbitpublishers.com/wp-content/uploads/2021/06/guia-para-abrir-un-consultorio-medico.jpg"/>
-          <div id="ideales" style={{marginTop: '10pt', marginBottom: '10pt'}}>
+          <div  className={classes.idealesCards} id="ideales" style={{marginTop: '10pt', marginBottom: '10pt'}}>
            <Fade bottom>
-            <p className={classes.subtitle1}>Misión</p>   
-            <p className={classes.text1}>Brindar a nuestros pacientes un estilo de vida saludable e incrementar su esperanza de vida  a través  de  una  atención  personalizada por profesionales médicos para mejorar sus condiciones de salud, bienestar y belleza tanto física como psicológica a través del compromiso de nuestro equipo.</p>
+            <Paper elevation={elevat} className={classes.card}>
+              <p className={classes.subtitle1}>Misión</p>
+              <p className={classes.text1}>Brindar a nuestros pacientes un estilo de vida saludable e incrementar su esperanza de vida  a través  de  una  atención  personalizada por profesionales médicos para mejorar sus condiciones de salud, bienestar y belleza tanto física como psicológica a través del compromiso de nuestro equipo.</p>
+            </Paper>
            </Fade>
            <Fade bottom>
-            <p className={classes.subtitle2}>Visión</p>   
-            <p className={classes.text1}>Buscar ser la organización mas reconocida a nivel nacional e internacional  por su aporte  en  salud, bienestar, belleza, calidad humana y asesoría médica contando con excelentes procedimientos, tecnológicos y productos, con el fin de consolidarse en una entidad prestadora de servicios médicos estéticos, cada vez más integral.</p>
+            <Paper elevation={elevat} className={classes.card2}>
+              <p className={classes.subtitle2}>Valores</p>   
+              <List style={{marginTop: '-10pt'}}>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Realizar nuestro trabajo con la pasión, dedicación y entusiasmo necesario para llevar a cabo nuestra misión.</p>
+                  </ListItem>
+                </Fade>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Honestidad, para transmitir confianza en todos los ámbitos ajustándonos siempre a la verdad.</p>
+                  </ListItem>
+                </Fade>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Respeto a nuestros pacientes mejorando su calidad de vida.</p>
+                  </ListItem>
+                </Fade>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Puntualidad para estar a tiempo y en el tiempo para satisfacción de nuestros pacientes.</p>
+                  </ListItem>
+                </Fade>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Amor para descubrir, aprender y compartir con cada uno de nuestros pacientes sus necesidades y disfrutarla por sus mejoras. </p>
+                  </ListItem>
+                </Fade>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Liderar,  motivar y compartir con nuestros pacientes la mejora de su calidad de vida. </p>
+                  </ListItem>
+                </Fade>
+                <Fade bottom>
+                  <ListItem>
+                    <p className={classes.text1}>• Confianza para darle la seguridad que su inquietud y necesidad será cubierta por nuestros profesionales a cada pacientes </p>
+                  </ListItem>
+                </Fade>
+              </List>
+            </Paper>
            </Fade>
            <Fade bottom>
-            <p className={classes.subtitle1}>Valores</p>   
-            <List style={{marginTop: '-10pt'}}>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Realizar nuestro trabajo con la pasión, dedicación y entusiasmo necesario para llevar a cabo nuestra misión.</p>
-                </ListItem>
-              </Fade>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Honestidad, para transmitir confianza en todos los ámbitos ajustándonos siempre a la verdad.</p>
-                </ListItem>
-              </Fade>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Respeto a nuestros pacientes mejorando su calidad de vida.</p>
-                </ListItem>
-              </Fade>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Puntualidad para estar a tiempo y en el tiempo para satisfacción de nuestros pacientes.</p>
-                </ListItem>
-              </Fade>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Amor para descubrir, aprender y compartir con cada uno de nuestros pacientes sus necesidades y disfrutarla por sus mejoras. </p>
-                </ListItem>
-              </Fade>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Liderar,  motivar y compartir con nuestros pacientes la mejora de su calidad de vida. </p>
-                </ListItem>
-              </Fade>
-              <Fade bottom>
-                <ListItem>
-                  <p className={classes.text1}>• Confianza para darle la seguridad que su inquietud y necesidad será cubierta por nuestros profesionales a cada pacientes </p>
-                </ListItem>
-              </Fade>
-            </List>
+            <Paper elevation={elevat} className={classes.card}>
+              <p className={classes.subtitle1}>Visión</p>   
+              <p className={classes.text1}>Buscar ser la organización mas reconocida a nivel nacional e internacional  por su aporte  en  salud, bienestar, belleza, calidad humana y asesoría médica contando con excelentes procedimientos, tecnológicos y productos, con el fin de consolidarse en una entidad prestadora de servicios médicos estéticos, cada vez más integral.</p>
+            </Paper>
            </Fade>
           </div>
         </div>
